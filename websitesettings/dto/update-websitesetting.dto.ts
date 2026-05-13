29cb3482f4b89f32bda69b70aa690996ad4aee95
Rifatul-Islam-Munna <islamrifat117@gmail.com>
2026-05-13 23:27:33 +0600

    updated

--- websitesettings/dto/update-websitesetting.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateWebsitesettingDto } from './create-websitesetting.dto';
-
-export class UpdateWebsitesettingDto extends PartialType(CreateWebsitesettingDto) {}
--- websitesettings/websitesettings.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { WebsitesettingsService } from './websitesettings.service';
-import { WebsitesettingsController } from './websitesettings.controller';
-
-@Module({
-  controllers: [WebsitesettingsController],
-  providers: [WebsitesettingsService],
-})
-export class WebsitesettingsModule {}
